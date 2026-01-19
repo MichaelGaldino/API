d5d659e7ebb6f3d10a360d76048a0bf48652c193
Michael Pedro <128324480+MichaelGaldino@users.noreply.github.com>
2026-01-19 17:10:25 -0300

    1

---
+++ WeatherAPI/main.py
@@ -0,0 +1,17 @@
+import requests
+import pprint
+
+api_key = "65072fed03ff4fba9a7193613261901"
+api_link = "http://api.weatherapi.com/v1/current.json"
+
+parametros ={
+    "key": api_key,
+    "q": "Paris",        
+    "lang": "pt"      
+}
+
+resposta = requests.get(api_link, params=parametros)
+
+if resposta.status_code == 200:
+    dados_requisicao = resposta.json()
+    pprint.pprint(dados_requisicao)
