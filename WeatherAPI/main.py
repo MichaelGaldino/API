e6f59e764e2780e869738256e363fec2983284dc
Michael Pedro <128324480+MichaelGaldino@users.noreply.github.com>
2026-01-19 17:34:11 -0300

    Aprendendo a navegar por api's

--- WeatherAPI/main.py
+++ WeatherAPI/main.py
@@ -4,14 +4,16 @@ import pprint
 api_key = "65072fed03ff4fba9a7193613261901"
 api_link = "http://api.weatherapi.com/v1/current.json"
 
-parametros ={
+parametros= {
     "key": api_key,
-    "q": "Paris",        
-    "lang": "pt"      
+    "q": "Japan",        
+    "lang": "pt",   
 }
 
 resposta = requests.get(api_link, params=parametros)
 
 if resposta.status_code == 200:
-    dados_requisicao = resposta.json()
-    pprint.pprint(dados_requisicao)
+    requisitado = resposta.json()
+    pprint.pprint(requisitado)
+    temp = requisitado['current']['condition']['text']
+    print(temp)
