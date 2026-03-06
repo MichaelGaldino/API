4151507ce1a58da1d663abe1f941ed484ac437ea
Michael Pedro <128324480+MichaelGaldino@users.noreply.github.com>
2026-03-06 08:30:58 -0300

    1

--- fastapi-aula/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-app = FastAPI()
-
-from rotas_autenticacao import roteador_autenticacao
-from rotas_pedidos import roteador_pedidos
-
-app.include_router(roteador_autenticacao)
-app.include_router(roteador_pedidos)
--- fastapi-aula/rotas_autenticacao.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-roteador_autenticacao = APIRouter(prefix="/auth", tags=["autenticacao"])
-
-@roteador_autenticacao.get("/")
-async def autenticar():
-    """
-    Essa é a rota de autenticação
-    """
-    return {"mensagem": "Você acessou a rota de autenticação", "autenticar": False}
